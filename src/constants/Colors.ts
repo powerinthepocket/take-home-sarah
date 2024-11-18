2c30873d4b3f2ae9d8d71d6f67494385637d32f8
Sliti Ahmed <asliti@tkyr.net>
2024-11-18 01:42:49 +0100

    Add  colors palette to colors.ts

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,19 +1,26 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
+    greyBG: "#ededed",
+    greyText: "#797777",
+    grey: "#555",
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
+    greyBG: "#ededed",
+    greyText: "#797777",
+    grey: "#555",
+
     tabIconSelected: tintColorDark,
   },
 };
