d9c2c4863661c7e3149698016512ab578a978a5a
Sliti Ahmed <asliti@tkyr.net>
2024-11-18 01:44:06 +0100

    Add PortfolioValue component

---
+++ src/component /PortfolioValue.tsx
@@ -0,0 +1,33 @@
+import React, { memo } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Colors from "@/src/constants/Colors";
+
+interface PortfolioValueCardProps {
+  value: string;
+  label: string;
+}
+const styles = StyleSheet.create({
+  portfolioSection: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  portfolioValue: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.light.text,
+  },
+});
+const PortfolioValue: React.FC<PortfolioValueCardProps> = memo(
+  ({ value, label }) => {
+    return (
+      <View style={styles.portfolioSection}>
+        <Text style={styles.portfolioValue}>{label}</Text>
+        <Text style={styles.portfolioValue}>{value}</Text>
+      </View>
+    );
+  }
+);
+
+export default PortfolioValue;
